5ec02b33a3428375d4aed029e0430f5b86f3a161
yeasin2448 <ayeasin108@gmail.com>
2023-02-02 17:54:24 +0600

    update hover component

--- src/components/HoverCounter.js
+++ src/components/HoverCounter.js
@@ -7,12 +7,12 @@ function HoverCounter({ count, incrementCount, theme, switchTheme }) {
     <div className="bg-white max-w-[500px] mx-auto drop-shadow-md rounded-md p-4">
       <h4
         onMouseOver={incrementCount}
-        className="text-xl font-sans font-bold text-black"
+        className="text-xl font-sans font-bold text-black rounded-md p-2"
         style={style}
       >
         Hello I am Yeasin arafat, I'm exist {count} time
       </h4>
-      <button type="button" onClick={switchTheme}>Change Theme</button>
+      <button type="button" onClick={switchTheme} className="bg-purple-500 py-2 px-8 rounded-md text-white mt-8">Change Theme</button>
     </div>
   );
 }
